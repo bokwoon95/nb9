e3cf8f83d74744410942bb0e4463334fd6545265
bokwoon <bokwoon.c@gmail.com>
2023-12-04 17:42:10 +0800

    checkpoint

--- fs.go
+++ fs.go
@@ -2,68 +2,57 @@ package nb9
 
 import (
 	"context"
-	"io"
 	"io/fs"
+	"time"
 )
 
-type NotebrewFS interface {
+type FS interface {
 	// WithContext returns a new FS with the given context.
-	NotebrewContext(context.Context) NotebrewFS
+	WithContext(context.Context) FS
 
 	// Open opens the named file.
 	Open(name string) (fs.File, error)
-	// - fs.ErrInvalid
-	// - fs.ErrNotExist
-	// - syscall.EISDIR (read file)
 
-	// OpenWriter opens an io.WriteCloser that represents an instance of a
-	// file. The parent directory must exist. If the file doesn't exist, it
-	// should be created. If the file exists, its should be truncated.
-	OpenWriter(name string, perm fs.FileMode) (io.WriteCloser, error)
-	// - fs.ErrInvalid
-	// - syscall.EISDIR
-	// - syscall.ENOTDIR (parent)
-	// - fs.ErrNotExist (parent)
-
-	// ReadDir reads the named directory and returns a list of directory
-	// entries sorted by filename.
-	ReadDir(name string) ([]fs.DirEntry, error)
-	// - fs.ErrInvalid
-	// - fs.ErrNotExist
-	// - syscall.ENOTDIR
+	Stat(name string) (FileInfo, error)
 
 	// Mkdir creates a new directory with the specified name.
 	Mkdir(name string, perm fs.FileMode) error
-	// - fs.ErrInvalid
-	// - fs.ErrNotExist (parent)
-	// - fs.ErrExist
 
 	// Remove removes the named file or directory.
 	Remove(name string) error
-	// - fs.ErrInvalid
-	// - fs.ErrNotExist (parent)
-	// - syscall.ENOTEMPTY
 
 	// Rename renames (moves) oldname to newname. If newname already exists and
 	// is not a directory, Rename replaces it.
 	Rename(oldname, newname string) error
-	// - fs.ErrInvalid
-	// - fs.ErrNotExist (oldname)
-	// - syscall.EISDIR (newname)
 }
 
-type NotebrewFile interface {
+// NOTE: what if you want to read a directory without opening a file, which
+// incurs a database lookup? Should (*RemoteFile).Read be lazily intialized? FS.OpenFile should be as cheap as possible?
+type File interface {
+	Stat() (FileInfo, error)
+	Read([]byte) (int, error)
+	Write([]byte) (int, error)
+	Close() error
+	NameFilter(before, after string, limit int) error
+	ReadDirList(batch []FileInfo) (n int, err error)
+	ReadDirTree(batch []FileInfo) (n int, err error)
 	// FilterByName(before, after string, limit int) // For sorting and filtering, optional and discoverable
 	// FilterByModificationTime(before, after time.Time, limit int) // For sorting and filtering, optional and discoverable
 	// ReadDirList([]DirEntry) (n int, err error) // For streaming
 	// ReadDirTree([]DirEntry) (n int, err error)
 }
 
-type NotebrewFileInfo interface {
-	// fs.FileInfo
-	// Count() (int64, error)
-	// Size() (int64, error)
+type FileInfo interface {
+	Name() string
+	Size() int64
+	Mode() fs.FileMode
+	ModTime() time.Time
+	IsDir() bool
+	Open() (fs.File, error)
+	Count() int64
 }
 
-type NotebrewDirEntry interface {
+type DirReader interface {
+	ReadDir(batch []FileInfo) (n int, err error)
+	Close() error
 }
