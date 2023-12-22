f890272f9c463785c716fcca5fc9d1d5839f9951
bokwoon <bokwoon.c@gmail.com>
2023-12-22 23:27:34 +0800

    checkpoint

--- site_generator.go
+++ site_generator.go
@@ -167,6 +167,11 @@ type PageData struct {
 	ModificationTime time.Time
 }
 
+// GeneratePage(file, childPages)
+// GeneratePost(file)
+// GeneratePostList(files)
+// TODO: figure out how to make GeneratePage and GeneratePost/GeneratePostList
+// workable using a single walk of the directory.
 func (siteGen *SiteGenerator) GeneratePage(ctx context.Context, name string, file fs.File) error {
 	var urlPath string
 	if name != "index.html" {
