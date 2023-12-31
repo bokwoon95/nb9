df297114943fe97b3546b39f1b32dbdd40f932fb
bokwoon <bokwoon.c@gmail.com>
2024-01-01 00:53:00 +0800

    checkpoint

--- file_handler.go
+++ file_handler.go
@@ -64,7 +64,7 @@ func (nbrew *Notebrew) fileHandler(w http.ResponseWriter, r *http.Request, usern
 	_ = fileType
 	// TODO: is the file editable?
 	// TODO: does the file have an output/parent link?
-	// TODO: $.ContentURL ()
+	// TODO: $.ContentURL (remove the ContentDomain field from every Response struct, replace it with ContentURL instead)
 	// back | files | <type> | <parent> | <view URL, belongs to page>
 }
 
