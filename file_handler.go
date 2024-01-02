3c9847ac0c6ae8f268cfe3f0b5b8c02dfed3600b
bokwoon <bokwoon.c@gmail.com>
2024-01-02 21:31:30 +0800

    checkpoint

--- file_handler.go
+++ file_handler.go
@@ -1,7 +1,9 @@
 package nb9
 
 import (
+	"encoding/json"
 	"errors"
+	"html/template"
 	"io/fs"
 	"net/http"
 	"path"
@@ -63,31 +65,14 @@ func (nbrew *Notebrew) fileHandler(w http.ResponseWriter, r *http.Request, usern
 		notFound(w, r)
 		return
 	}
-	if !strings.Contains(filePath, "/") {
+
+	var isEditable bool
+	n := strings.Index(filePath, "/")
+	if n < 0 {
 		notFound(w, r)
 		return
 	}
-
-	var response fileResponse
-	if r.Method == "GET" {
-		_, err = nbrew.getSession(r, "flash", &response)
-		if err != nil {
-			getLogger(r.Context()).Error(err.Error())
-		}
-		nbrew.clearSession(w, r, "flash")
-		if response.Status == "" {
-			response.Status = GetSuccess // TODO: do it like this? set status here?
-		}
-	}
-	response.ContentSite = nbrew.contentSite(sitePrefix)
-	response.Username = username
-	response.SitePrefix = sitePrefix
-	response.Path = filePath
-	response.IsDir = fileInfo.IsDir()
-	response.ModTime = fileInfo.ModTime()
-
-	var isEditable bool
-	head, tail, _ := strings.Cut(filePath, "/")
+	head, tail := filePath[:n], filePath[n+1:]
 	switch head {
 	case "notes":
 		isEditable = fileType.Ext == ".html" || fileType.Ext == ".css" || fileType.Ext == ".js" || fileType.Ext == ".md" || fileType.Ext == ".txt"
@@ -97,13 +82,6 @@ func (nbrew *Notebrew) fileHandler(w http.ResponseWriter, r *http.Request, usern
 			notFound(w, r)
 			return
 		}
-		if tail == "index.html" {
-			response.AssetDir = "output"
-			response.URL = response.ContentSite
-		} else {
-			response.AssetDir = path.Join("output", strings.TrimSuffix(tail, ".html"))
-			response.URL = response.ContentSite + "/" + strings.TrimSuffix(tail, ".html") + "/"
-		}
 	case "posts":
 		isEditable = fileType.Ext == ".md"
 		if !isEditable {
@@ -111,18 +89,18 @@ func (nbrew *Notebrew) fileHandler(w http.ResponseWriter, r *http.Request, usern
 			return
 		}
 	case "output":
-		next, _, _ := strings.Cut(tail, "/")
-		switch next {
+		n := strings.Index(tail, "/")
+		if n < 0 {
+			notFound(w, r)
+			return
+		}
+		switch tail[:n] {
 		case "posts":
 			isEditable = false
 		case "themes":
 			isEditable = fileType.Ext == ".html" || fileType.Ext == ".css" || fileType.Ext == ".js" || fileType.Ext == ".md" || fileType.Ext == ".txt"
 		default:
 			isEditable = fileType.Ext == ".css" || fileType.Ext == ".js" || fileType.Ext == ".md"
-			if isEditable {
-				// output/foo/bar/baz.js => pages/foo/bar.html
-				response.BelongsTo = path.Dir(tail) + ".html"
-			}
 		}
 	default:
 		notFound(w, r)
@@ -136,7 +114,97 @@ func (nbrew *Notebrew) fileHandler(w http.ResponseWriter, r *http.Request, usern
 
 	switch r.Method {
 	case "GET":
+		var response fileResponse
+		_, err = nbrew.getSession(r, "flash", &response)
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+		}
+		nbrew.clearSession(w, r, "flash")
+		if response.Status == "" {
+			response.Status = GetSuccess // TODO: do it like this? set status here?
+		}
+		response.ContentSite = nbrew.contentSite(sitePrefix)
+		response.Username = username
+		response.SitePrefix = sitePrefix
+		response.Path = filePath
+		response.IsDir = fileInfo.IsDir()
+		response.ModTime = fileInfo.ModTime()
+		switch head {
+		case "pages":
+			if tail == "index.html" {
+				response.AssetDir = "output"
+				response.URL = response.ContentSite
+			} else {
+				response.AssetDir = path.Join("output", strings.TrimSuffix(tail, ".html"))
+				response.URL = response.ContentSite + "/" + strings.TrimSuffix(tail, ".html") + "/"
+			}
+			// TODO: based on RemoteFS or not, fill in the Assets
+		case "posts":
+			response.AssetDir = path.Join("output", strings.TrimSuffix(filePath, ".md"))
+			response.URL = response.ContentSite + "/" + strings.TrimSuffix(filePath, ".md") + "/"
+			// TODO: based on RemoteFS or not, fill in the Assets
+		case "output":
+			n := strings.Index(tail, "/")
+			if tail[:n] != "posts" && tail[:n] != "themes" && isEditable {
+				response.BelongsTo = path.Join("pages", path.Dir(tail)+".html")
+			}
+		}
+		if r.Form.Has("api") {
+			w.Header().Set("Content-Type", "application/json")
+			encoder := json.NewEncoder(w)
+			encoder.SetEscapeHTML(false)
+			err := encoder.Encode(&response)
+			if err != nil {
+				getLogger(r.Context()).Error(err.Error())
+			}
+			return
+		}
+		if isEditable {
+			referer := getReferer(r)
+			funcMap := map[string]any{
+				"join":             path.Join,
+				"dir":              path.Dir,
+				"base":             path.Base,
+				"ext":              path.Ext,
+				"hasPrefix":        strings.HasPrefix,
+				"hasSuffix":        strings.HasSuffix,
+				"trimPrefix":       strings.TrimPrefix,
+				"contains":         strings.Contains,
+				"fileSizeToString": fileSizeToString,
+				"stylesCSS":        func() template.CSS { return template.CSS(stylesCSS) },
+				"baselineJS":       func() template.JS { return template.JS(baselineJS) },
+				// "contentURL":       func() string { return contentURL },
+				"hasDatabase": func() bool { return nbrew.UsersDB != nil },
+				"referer":     func() string { return referer },
+				"safeHTML":    func(s string) template.HTML { return template.HTML(s) },
+				// "pagePath":         func() string { return pagePath },
+				// "pageURL":          func() string { return pageURL },
+				// "postURL":          func() string { return postURL },
+				"head": func(s string) string {
+					head, _, _ := strings.Cut(s, "/")
+					return head
+				},
+				"tail": func(s string) string {
+					_, tail, _ := strings.Cut(s, "/")
+					return tail
+				},
+			}
+			tmpl, err := template.New("file.html").Funcs(funcMap).ParseFS(rootFS, "embed/file.html")
+			if err != nil {
+				getLogger(r.Context()).Error(err.Error())
+				internalServerError(w, r, err)
+				return
+			}
+			w.Header().Set("Content-Security-Policy", contentSecurityPolicy)
+			executeTemplate(w, r, fileInfo.ModTime(), tmpl, &response)
+			return
+		}
+		// TODO: if we reach here, it means the file is not editable and we serve the file directly. If not a RemoteFile, we can just reuse serveFile. Otherwise, we do custom logic to serve the RemoteFile that we already have.
 	case "POST":
+		if !isEditable {
+			methodNotAllowed(w, r)
+			return
+		}
 	default:
 		methodNotAllowed(w, r)
 	}
