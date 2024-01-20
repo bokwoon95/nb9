52c17818bb252c77c257ff38d0cd0327053147bc
bokwoon <bokwoon.c@gmail.com>
2024-01-20 13:10:38 +0800

    checkpoint

--- creation_time_unix.go
+++ creation_time_unix.go
@@ -1,1 +1,14 @@
+//go:build unix
+
 package nb9
+
+import (
+	"io/fs"
+	"syscall"
+	"time"
+)
+
+func getCreationTime(filePath string, fileInfo fs.FileInfo) (time.Time, error) {
+	stat := fileInfo.Sys().(*syscall.Stat_t)
+	return time.Unix(stat.Birthtimespec.Sec, stat.Birthtimespec.Nsec), nil
+}
