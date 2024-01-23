1f19db9637c8b4dab1e27afc32fba866ca7f7fde
bokwoon <bokwoon.c@gmail.com>
2024-01-23 17:52:32 +0800

    checkpoint

--- search.go
+++ search.go
@@ -6,16 +6,14 @@ import (
 	"net/http"
 	"path"
 	"strings"
+
+	"github.com/bokwoon95/nb9/sq"
 )
 
 func (nbrew *Notebrew) search(w http.ResponseWriter, r *http.Request, username, sitePrefix string) {
-	type Match struct {
-		LineNumber int        `json:"lineNumber"`
-		Lines      [][]string `json:"lines,omitempty"`
-	}
 	type File struct {
-		FilePath string  `json:"filePath"`
-		Matches  []Match `json:"matches,omitempty"`
+		FilePath string `json:"filePath"`
+		Preview  string `json:"preview"`
 	}
 	type Response struct {
 		Error       string     `json:"error,omitempty"`
@@ -67,7 +65,6 @@ func (nbrew *Notebrew) search(w http.ResponseWriter, r *http.Request, username, 
 		notFound(w, r)
 		return
 	}
-	_ = remoteFS
 
 	writeResponse := func(w http.ResponseWriter, r *http.Request, response Response) {
 		if r.Form.Has("api") {
@@ -103,6 +100,42 @@ func (nbrew *Notebrew) search(w http.ResponseWriter, r *http.Request, username, 
 	if response.Search == "" {
 		writeResponse(w, r, response)
 		return
+	}
+	var err error
+	switch remoteFS.filesDialect {
+	case "sqlite":
+		var siteFilter sq.Expression
+		if sitePrefix != "" {
+			siteFilter = sq.Expr("(file_path LIKE {} ESCAPE '\\')", sitePrefix+"/%")
+		} else {
+			siteFilter = sq.Expr("(file_path LIKE 'notes/%'" +
+				" OR file_path LIKE 'pages/%'" +
+				" OR file_path LIKE 'posts/%'" +
+				" OR file_path LIKE 'output/%')")
+		}
+		response.Files, err = sq.FetchAll(r.Context(), remoteFS.filesDB, sq.Query{
+			Dialect: remoteFS.filesDialect,
+			Format: "SELECT {*}" +
+				" FROM files" +
+				" JOIN files_fts5 ON files_fts5.rowid = files.rowid" +
+				" WHERE {siteFilter}" +
+				" AND files_fts5.text MATCH {searchTerm}" +
+				" ORDER BY files_fts5.rank",
+			Values: []any{
+				sq.Param("siteFilter", siteFilter),
+				sq.StringParam("searchTerm", `"`+strings.ReplaceAll(response.Search, `"`, `""`)+`"`),
+			},
+		}, func(row *sq.Row) File {
+			return File{
+				FilePath: row.String("files.file_path"),
+				Preview:  row.String("substr(files.text, 1, 500)"),
+			}
+		})
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+			internalServerError(w, r, err)
+			return
+		}
 	}
 	writeResponse(w, r, response)
 }
